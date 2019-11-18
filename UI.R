19e66c584ec3d3e36af08f7881b24a34da0488e8
Patryk Przybyszewski <superkarmel123@gmail.com>
2019-11-18 09:47:27 +0100

    Dodano liste lat do wyboru do wykresu top 10

--- UI.R
+++ UI.R
@@ -26,7 +26,9 @@ ui <- fluidPage(
     mainPanel(
       textOutput("text"),
       # Output: Histogram ----
-      plotOutput(outputId = "distPlot")
+      plotOutput(outputId = "distPlot"),
+      
+      selectInput(inputId = "wyborRoku", label = "Wybierz rok:", choices = list("2000" = 2000, "2001" = 2001, "2002" = 2002, "2003" = 2003, "2004" = 2004, "2005" = 2005, "2006" = 2006, "2007" = 2007, "2008" = 2008, "2009" = 2009, "2010" = 2010, "2011" = 2011, "2012" = 2012, "2013" = 2013, "2014" = 2014, "2015" = 2015, "2016" = 2016, "2017" = 2017, "2018" = 2018), , selected = NULL, multiple = FALSE, selectize = FALSE)
       
     )
   )
