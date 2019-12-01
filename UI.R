731b82c848371348b8cfa9c25c75f0ea8608685a
Radoslaw_Siwinski <r.silacz@wp.pl>
2019-12-01 18:29:11 +0100

    guzik

--- UI.R
+++ UI.R
@@ -30,6 +30,8 @@ ui <- fluidPage(
         tabPanel("Wyszukiwanie imion",
                  fluidRow(
                    column(8,textInput("imie","Wpisz imie",value = "ANDRZEJ")),
+                   column(9,actionButton("go", "PLOT")),
+                   column(10,textOutput("out")),
                    column(12,plotOutput(outputId = "distPlot")))),
         tabPanel("Top 10",wyborroku<-selectInput(inputId = "wyborRoku", label = "Wybierz rok:", choices = list("2000" = 2000, "2001" = 2001, "2002" = 2002, "2003" = 2003, "2004" = 2004, "2005" = 2005, "2006" = 2006, "2007" = 2007, "2008" = 2008, "2009" = 2009, "2010" = 2010, "2011" = 2011, "2012" = 2012, "2013" = 2013, "2014" = 2014, "2015" = 2015, "2016" = 2016, "2017" = 2017, "2018" = 2018), , selected = NULL, multiple = FALSE, selectize = FALSE), ),
         tabPanel("Rok",wyborroku )
