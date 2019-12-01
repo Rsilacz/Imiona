08aa79591ec98b1904a360a422024b2b6ed3fdf1
Patryk Przybyszewski <superkarmel123@gmail.com>
2019-12-01 16:08:51 +0100

    Widok kart

--- UI.R
+++ UI.R
@@ -7,12 +7,12 @@ ui <- fluidPage(
   titlePanel("Imiona dzieci w Polsce w latach 2000-2018"),
   
   # Sidebar layout with input and output definitions ----
-  sidebarLayout(
+  sidebarLayout(NULL,
     
     # Sidebar panel for inputs ----
-    sidebarPanel(
-      
-      textInput("imie","Wpisz imie",value = "ANDRZEJ")
+    #sidebarPanel(
+     
+     
       # Input: Slider for the number of bins ----
      # sliderInput(inputId = "bins",
        #           label = "Number of bins:",
@@ -20,15 +20,25 @@ ui <- fluidPage(
       #            max = 2000,
        #           value = 30)
       
-    ),
+  # ),
+    
+    
     
     # Main panel for displaying outputs ----
     mainPanel(
-      textOutput("text"),
+      tabsetPanel(
+        tabPanel("Wyszukiwanie imion",
+                 fluidRow(
+                   column(8,textInput("imie","Wpisz imie",value = "ANDRZEJ")),
+                   column(12,plotOutput(outputId = "distPlot")))),
+        tabPanel("Top 10",wyborroku<-selectInput(inputId = "wyborRoku", label = "Wybierz rok:", choices = list("2000" = 2000, "2001" = 2001, "2002" = 2002, "2003" = 2003, "2004" = 2004, "2005" = 2005, "2006" = 2006, "2007" = 2007, "2008" = 2008, "2009" = 2009, "2010" = 2010, "2011" = 2011, "2012" = 2012, "2013" = 2013, "2014" = 2014, "2015" = 2015, "2016" = 2016, "2017" = 2017, "2018" = 2018), , selected = NULL, multiple = FALSE, selectize = FALSE), ),
+        tabPanel("Rok",wyborroku )
+      ),
+      textOutput("text")
       # Output: Histogram ----
-      plotOutput(outputId = "distPlot"),
+     # plotOutput(outputId = "distPlot"),
       
-      selectInput(inputId = "wyborRoku", label = "Wybierz rok:", choices = list("2000" = 2000, "2001" = 2001, "2002" = 2002, "2003" = 2003, "2004" = 2004, "2005" = 2005, "2006" = 2006, "2007" = 2007, "2008" = 2008, "2009" = 2009, "2010" = 2010, "2011" = 2011, "2012" = 2012, "2013" = 2013, "2014" = 2014, "2015" = 2015, "2016" = 2016, "2017" = 2017, "2018" = 2018), , selected = NULL, multiple = FALSE, selectize = FALSE)
+      
       
     )
   )
