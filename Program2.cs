76cf7b9f586d2710f85b189792cbe40ddb94e463
291588 <opss4@wp.pl>
2020-01-24 18:28:02 +0100

    usuwanie_polskich_liter

---
+++ Program2.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Office.Interop.Excel;
+using Excel = Microsoft.Office.Interop.Excel;
+
+namespace przerobka
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Excel.Application app = new Excel.Application();
+
+            Excel.Workbook workbook1;
+            workbook1 = app.Workbooks.Open("C:\\Imiona\\Imiona\\im_2017.xlsx");
+            Excel.Worksheet input = workbook1.Sheets[1];
+
+            int i = 2;
+            string imie;
+            while (input.Cells[1, i].Value != null)
+            {
+                Console.WriteLine("Imie nr{0}", i);
+                imie = input.Cells[1, i].Value;
+                imie = imie.Replace("Ł", "L");
+                imie = imie.Replace("Ą", "A");
+                imie = imie.Replace("Ę", "E");
+                imie = imie.Replace("Ć", "C");
+                imie = imie.Replace("Ń", "N");
+                imie = imie.Replace("Ó", "O");
+                imie = imie.Replace("Ś", "S");
+                imie = imie.Replace("Ź", "Z");
+                imie = imie.Replace("Ż", "Z");
+                input.Cells[1, i].Value = imie;
+
+                i++;
+            }
+
+            Console.WriteLine("DONE");
+            
+
+
+
+
+            Console.ReadKey();
+
+
+
+            workbook1.Close(true);
+            app.Quit();
+
+            Marshal.ReleaseComObject(app);
+
+        }
+    }
+}
